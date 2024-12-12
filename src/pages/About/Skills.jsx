43bf4223328e17122c485fc7358b0370046992b0
Shoko <97951985+Shokoloa@users.noreply.github.com>
2024-12-12 17:49:09 +0100

    Add files via upload

--- src/pages/About/Skills.jsx
+++ src/pages/About/Skills.jsx
@@ -1,10 +1,12 @@
-import html5 from '../../assets/images/icons/html5.png';
-import css3 from '../../assets/images/icons/css3.png';
-import javascript from '../../assets/images/icons/javascript.png';
-import python from '../../assets/images/icons/python.png';
-import arduino from '../../assets/images/icons/arduino.png';
-import vite from '../../assets/images/icons/vite.png';
-import djs from '../../assets/images/icons/djs.png';
+import html5 from '../../assets/images/icons/html5.webp';
+import css3 from '../../assets/images/icons/css3.webp';
+import javascript from '../../assets/images/icons/javascript.webp';
+import python from '../../assets/images/icons/python.webp';
+import vite from '../../assets/images/icons/vite.webp';
+import djs from '../../assets/images/icons/djs.webp';
+import FlagFR from '../../assets/images/icons/flag_fr.webp';
+import FlagBR from '../../assets/images/icons/flag_br.webp';
+import FlagSP from '../../assets/images/icons/flag_sp.webp';
 
 const getImage = (imageName) => {
     switch (imageName) {
@@ -16,12 +18,16 @@ const getImage = (imageName) => {
             return javascript;
         case 'python':
             return python;
-        case 'arduino':
-            return arduino;
         case 'vite':
             return vite;
         case 'djs':
             return djs;
+        case 'fr':
+            return FlagFR;
+        case 'en':
+            return FlagBR;
+        case 'es':
+            return FlagSP;
         default:
             return null;
     }
@@ -32,11 +38,12 @@ export const Skills = ({ data }) => {
         <div className="skills-list">
             {data.map((skill, index) => {
                 const image = getImage(skill.img);
+                if (!image) return;
 
                 return (
                     <article className="skill" key={index}>
                         <img src={image} draggable={false} width={50} height={50} alt="Skill" />
-                        <p>{skill.name}<br /><span>{skill.level}</span></p>
+                        <span className="skill-tooltip">{skill.name} | {skill.level}</span>
                     </article>
                 );
             })}
